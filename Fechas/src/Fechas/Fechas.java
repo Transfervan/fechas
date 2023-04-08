0fb27d47bc1609f0091c02742f6cbc2e94fe7af4
Transfervan <129233255+Transfervan@users.noreply.github.com>
2023-04-08 16:48:00 -0400

    version 2.0

--- Fechas/src/Fechas/Fechas.java
+++ Fechas/src/Fechas/Fechas.java
@@ -2,6 +2,7 @@ package Fechas;
 
 
 import java.util.Scanner;
+import java.lang.*;
 
 public class Fechas {
 	
@@ -10,47 +11,82 @@ public class Fechas {
 		// TODO Auto-generated method stub
 		
 		Scanner teclado= new Scanner(System.in);
-		String fecha1 = "20-10-2016";
-		String fecha2 = "20-10-2016";
-		   System.out.println("Ingresar fechas de nacimiento de dos pesonas");
-		
-		    System.out.println("Ingrese Fecha en este formato 20-10-2016 Persona 1: ");
-	       fecha1=teclado.next();
-	        
+		Scanner teclado1= new Scanner(System.in);
+		String fecha1 = "20/10/2016";
+		String fecha2 = "20/10/2016";
+		 boolean correcto = false;
+		 int dia1 = 0;
+		 int mes1 = 0;
+		 int ano1 = 0;
+		 int dia2 = 0;
+		 int mes2 = 0;
+		 int ano2 = 0;
+		 		 
+	       do{
+	    	    System.out.println("Ingresar fechas de nacimiento de dos pesonas");
+	   		
+	  		    System.out.println("Ingrese Fecha en este formato 20/10/2016 Persona 1: ");
+	  		    fecha1=teclado.next();
+	  		   
+	 	       if (fecha1.length() == 10)
+	 	       	    if ((fecha1.substring(0,2)).matches("[0-9]*") & (fecha1.substring(2,3)).matches("/") & (fecha1.substring(3,5)).matches("[0-9]*") & (fecha1.substring(5,6)).matches("/") & (fecha1.substring(6,10)).matches("[0-9]*")) {
+	 	    	        correcto=true;
+	 	    	        } else {
+	 	    	        correcto=false;
+	 	    	       //System.out.println("El número introducido es " + fecha1.substring(0,2) + fecha1.substring(3,5) + fecha1.substring(6,10));
+	 	    	        System.out.println("Repite de nuevo");
+	 	    	    }
+
+	 	    	    }while(correcto==false);
+	            
 	      	   
+	    // extrae de string el dia
+ 	       String sSubCadena = fecha1.substring(0,2);
+ 	       dia1 = Integer.parseInt(sSubCadena); 
+ 	      
+ 	       String sSubCadena1 = fecha1.substring(3,5);
+ 	       mes1 = Integer.parseInt(sSubCadena1); 
+ 	    
+ 	       String sSubCadena2 = fecha1.substring(6,10);
+ 	       ano1 = Integer.parseInt(sSubCadena2); 
+ 	      
+	    
+ 	      boolean correcto2 = false;
+	 		 
+	       do{
+	    	     		
+	  		    System.out.println("Ingrese Fecha en este formato 20/10/2016 Persona 2: ");
+	  		    fecha2=teclado1.next();
+	  		   
+	  		  if (fecha2.length() == 10)
+	 	       	    if ((fecha2.substring(0,2)).matches("[0-9]*") & (fecha2.substring(2,3)).matches("/") & (fecha2.substring(3,5)).matches("[0-9]*") & (fecha2.substring(5,6)).matches("/") & (fecha2.substring(6,10)).matches("[0-9]*")) {
+	 	    	        correcto2=true;
+	 	    	        } else {
+	 	    	        correcto2=false;
+	 	    	       System.out.println("El número introducido es " + fecha2.substring(0,2) + fecha2.substring(3,5) + fecha2.substring(6,10));
+	 	    	        System.out.println("Repite de nuevo");
+	 	    	    }
+
+	 	    	    }while(correcto2==false);
 	       
-	       // extrae de string el dia
-	       String sSubCadena = fecha1.substring(0,2);
-	    // convierte string en integro
-	       int dia1 = Integer.parseInt(sSubCadena); 
-	      
-	       String sSubCadena1 = fecha1.substring(3,5);
-	     
-	       int mes1 = Integer.parseInt(sSubCadena1); 
-	       String sSubCadena2 = fecha1.substring(6,10);
-	      
-	       int ano1 = Integer.parseInt(sSubCadena2); 
-	       
-	       System.out.println("Ingrese Fecha en este formato 20-10-2016 Persona 2: ");
-	       fecha2=teclado.next();
-	       
-	       // extrae de string el dia
+	      	   
+	    // extrae de string el dia
 	       String sSubCadena3 = fecha2.substring(0,2);
-	    // convierte string en integro
-	       int dia2 = Integer.parseInt(sSubCadena3); 
-	       
+	       dia2 = Integer.parseInt(sSubCadena3); 
+	      
 	       String sSubCadena4 = fecha2.substring(3,5);
-	       
-	       int mes2 = Integer.parseInt(sSubCadena4); 
+	       mes2 = Integer.parseInt(sSubCadena4); 
+	    
 	       String sSubCadena5 = fecha2.substring(6,10);
+	       ano2 = Integer.parseInt(sSubCadena5); 
 	       
-	       int ano2 = Integer.parseInt(sSubCadena5); 
 	       
 	      if(ano1==ano2 &&	mes1==mes2 && dia1==dia2)
-	    	  	System.out.print("Tienen la misma edad");
+	    	  	System.out.print("Tienen la misma edad.");
 	      else 
 	    	  if(ano1>ano2)
 	    	      System.out.print("Persona 2 es mayor  ");
+	    	  else
 	    	  if(ano2>ano1)   
 	    	      System.out.print("Persona 1 es mayor  ");     
 	    	  		if(mes1>mes2)
@@ -65,6 +101,13 @@ public class Fechas {
 	    	 
 }
 
+//	private static int isNumeric(String sSubCadena1) {
+//		 TODO Auto-generated method stub
+//		return 0;
+	}
 
-    }
+
+
+
+    
 
